fb8ecfc228fe0aac787f938da1e14f094108ba64
codecraftsmanpawan <161737976+codecraftsmanpawan@users.noreply.github.com>
2025-01-20 11:27:35 +0530

    up

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -6,313 +6,45 @@ function Gallery() {
   useEffect(() => {
     document.title = "Gallery - Aharada Education";
   }, []);
+
+  // Inline style for images
+  const imageStyle = {
+    width: "100%",
+    height: "auto",
+    maxHeight: "320px",
+    objectFit: "cover",
+  };
+
   return (
     <div>
       <main>
         <Breadcrumbs />
         <div className="space">
-          <div className="container">
+          <div className="container px-3">
+            {" "}
+            {/* Added padding for equal spacing */}
             <div className="row gy-4 masonary-active">
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_1.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_1.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Digital Marketing</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_2.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_2.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Web Development</h2>
+              {[...Array(20)].map((_, index) => (
+                <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={index}>
+                  <div className="gallery-card">
+                    <div className="gallery-img">
+                      <img
+                        src={`assets/images/${index + 1}.png`}
+                        alt={`gallery image ${index + 1}`}
+                        style={imageStyle}
+                      />
+                      <a
+                        href={`assets/images/${index + 1}.png`}
+                        className="gallery-btn popup-image"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className="fas fa-eye" />
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_3.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_3.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Product Marketing</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_4.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_4.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Data Analysis</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_5.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_5.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Brand Growth All</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_6.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_6.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Content Writing</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_7.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_7.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">UI/UX Design</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_8.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_8.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">JavaScript</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_9.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_9.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Wordpress Programing</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_1.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_1.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Digital Marketing</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_2.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_2.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Web Development</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_3.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_3.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Product Marketing</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_4.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_4.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Data Analysis</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_5.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_5.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Brand Growth All</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6 col-xxl-auto filter-item">
-                <div className="gallery-card">
-                  <div className="gallery-img">
-                    <img
-                      src="assets/img/gallery/gallery_1_6.jpg"
-                      alt="gallery image"
-                    />{" "}
-                    <a
-                      href="assets/img/gallery/gallery_1_6.jpg"
-                      className="gallery-btn popup-image"
-                    >
-                      <i className="fas fa-eye" />
-                    </a>
-                  </div>
-                  <div className="gallery-content">
-                    <span className="gallery-tag">IT Solution</span>
-                    <h2 className="gallery-title">Big Data Science</h2>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
