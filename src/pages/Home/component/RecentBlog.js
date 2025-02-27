8cc1b16c7eba93670d1443491f7834db9170ac15
codecraftsmanpawan <161737976+codecraftsmanpawan@users.noreply.github.com>
2025-02-27 16:18:48 +0530

    up

--- src/pages/Home/component/RecentBlog.js
+++ src/pages/Home/component/RecentBlog.js
@@ -6,23 +6,20 @@ import { Link } from "react-router-dom";
 const BlogSection = () => {
   const [blogs, setBlogs] = useState([]);
 
+  // Fetch blog posts on component mount
   useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        const response = await axios.get(
-          "https://backend.aharadaedu.in/api/blogs"
+    axios
+      .get("https://backend.aharadaedu.in/api/blogs")
+      .then((response) => {
+        // Sort blog posts by publishedDate in descending order
+        const sortedBlogs = response.data.blogPosts.sort(
+          (a, b) => new Date(b.publishedDate) - new Date(a.publishedDate)
         );
-        if (response.data.blogPosts) {
-          const sortedBlogs = response.data.blogPosts.sort(
-            (a, b) => new Date(b.publishedDate) - new Date(a.publishedDate)
-          );
-          setBlogs(sortedBlogs);
-        }
-      } catch (error) {
+        setBlogs(sortedBlogs); // Set sorted blog posts
+      })
+      .catch((error) => {
         console.error("Error fetching blog posts:", error);
-      }
-    };
-    fetchBlogs();
+      });
   }, []);
 
   const settings = {
@@ -31,7 +28,7 @@ const BlogSection = () => {
     speed: 500,
     autoplay: true,
     autoplaySpeed: 3000,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [
       {
@@ -63,63 +60,72 @@ const BlogSection = () => {
               </div>
             </div>
             <div className="col-md-auto">
-              <Link to="/blogs" className="th-btn">
+              <a href="/blogs" className="th-btn">
                 View All Posts
                 <i className="fa-solid fa-arrow-right ms-2"></i>
-              </Link>
+              </a>
             </div>
           </div>
         </div>
 
-        {blogs.length > 0 ? (
-          <Slider
-            {...settings}
-            className="slider-shadow th-carousel blog-slider-1"
-          >
-            {blogs.slice(0, 5).map((blog) => (
-              <div className="col-md-6 col-xl-4" key={blog._id}>
-                <div className="th-blog blog-single style2">
-                  <div className="blog-img">
-                    <Link to={`/blogDetails/${blog._id}`}>
-                      <img
-                        src={`https://backend.aharadaedu.in${blog.image}`}
-                        alt={blog.title}
-                        style={{
-                          width: "100%",
-                          height: "220px",
-                          objectFit: "cover",
-                        }}
-                      />
-                    </Link>
-                  </div>
-                  <div className="blog-content">
-                    <div className="blog-meta">
-                      <Link to={`/blogDetails/${blog._id}`}>
-                        <i className="fa-light fa-clock"></i>
-                        {new Date(blog.publishedDate).toLocaleDateString()}
-                      </Link>
-                    </div>
-                    <h4
-                      className="box-title"
+        <Slider
+          {...settings}
+          className="slider-shadow th-carousel blog-slider-1"
+        >
+          {blogs.slice(0, 5).map((blog) => (
+            <div className="col-md-6 col-xl-4" key={blog._id}>
+              <div className="th-blog blog-single style2">
+                <div className="blog-img">
+                  <Link to={`/blogDetails/${blog._id}`}>
+                    <img
+                      src={`https://backend.aharadaedu.in${blog.image}`}
+                      alt={blog.title}
                       style={{
-                        WebkitLineClamp: 2,
-                        WebkitBoxOrient: "vertical",
+                        width: "100%",
+                        height: "220px",
+                        objectFit: "cover",
                       }}
-                    >
-                      <Link to={`/blogDetails/${blog._id}`}>{blog.title}</Link>
-                    </h4>
-                    <Link className="link-btn" to={`/blogDetails/${blog._id}`}>
-                      Read More Details
-                      <i className="fas fa-arrow-right ms-2"></i>
+                    />
+                  </Link>
+                </div>
+                <div
+                  className="blog-content"
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    height: "100%",
+                  }}
+                >
+                  <div className="blog-meta">
+                    <Link to={`/blogDetails/${blog._id}`}>
+                      <i className="fa-light fa-clock"></i>
+                      {new Date(blog.publishedDate).toLocaleDateString()}
                     </Link>
                   </div>
+                  <h4
+                    className="box-title"
+                    style={{
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      whiteSpace: "normal",
+                      display: "-webkit-box",
+                      WebkitLineClamp: 2,
+                      WebkitBoxOrient: "vertical",
+                      maxWidth: "100%",
+                    }}
+                  >
+                    <Link to={`/blogDetails/${blog._id}`}>{blog.title}</Link>
+                  </h4>
+                  <Link className="link-btn" to={`/blogDetails/${blog._id}`}>
+                    Read More Details
+                    <i className="fas fa-arrow-right ms-2"></i>
+                  </Link>
                 </div>
               </div>
-            ))}
-          </Slider>
-        ) : (
-          <p className="text-center">No blogs available.</p>
-        )}
+            </div>
+          ))}
+        </Slider>
       </div>
     </section>
   );
