96524caf17a3eb94a4278c5e63d64bcf194a546e
codecraftsmanpawan <161737976+codecraftsmanpawan@users.noreply.github.com>
2025-03-24 13:50:31 +0530

    up

--- src/pages/Programs/mba-aviation-management.js
+++ src/pages/Programs/mba-aviation-management.js
@@ -415,8 +415,8 @@ const Landingpage = () => {
                 <div className="department-details">
                   <div className="department-details-img mb-30">
                     <img
-                      src="../assets/program/aharadaedu2.png"
-                      alt="thumb"
+                      src="../assets/program/aharadaedu2.PNG"
+                      alt="img"
                       style={{
                         width: "100%",
                         height: "565px",
